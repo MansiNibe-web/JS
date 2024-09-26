38a472ed96bb97cbe3991bab6e624b5a209024e3
MansiNibe-web <mansinibe03@gmail.com>
2024-09-26 12:13:29 +0530

    Find program

--- demo.js
+++ demo.js
@@ -1,2 +1,39 @@
 
 console.log("first demo !");
+
+
+// array in js
+
+let numbers=[1,2,3,4,5,6,7,8,9];
+
+//find method in js 
+//it takes a function 
+
+numbers.find((n)=>{
+    if(n%2==0)
+        console.log("even");
+    else{
+        console.log("odd");
+        
+    }
+        
+})
+
+//without console.log
+
+console.log(numbers.find((n)=>{
+    if (n>5)
+    {
+    return n;
+    }
+}));
+
+console.log(numbers.find((n)=>
+{
+    if(n>5)
+    {
+        return n;   // return gives only single value as a output
+    }
+}));
+
+
