5ef4c31de93dd5d2ab6fd4ab8968c2d769a96177
MansiNibe-web <mansinibe03@gmail.com>
2024-09-27 12:27:36 +0530

    Tostring method

---
+++ ToString.js
@@ -0,0 +1,17 @@
+let demo=["mansi", "Nikita","Sankisha",["ramu",'Raju',"rakesh"]];
+console.log(demo);
+
+
+
+//tostring method is use to convert the array values into string 
+let strNames=demo.toString();
+console.log(strNames);
+
+//after run  it shows the result as ...........
+//mansi,Nikita,sankisha,ramu,Raju,ramesh........
+
+let d=[1,2,3,4,5,6,7,8];
+console.log(d);
+
+let s=d.toString();
+console.log(s);
