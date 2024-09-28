e6ebb506f058a1c92355055e3b1a9ef1cfbc46a0
MansiNibe-web <mansinibe03@gmail.com>
2024-09-28 15:21:07 +0530

    array methods

---
+++ Keys.js
@@ -0,0 +1,23 @@
+
+
+//keys return the index of the values 
+//it gives the iterator so we can perform iteratin on keys and values 
+let arr=[10,20,30,40,50];
+console.log("Index of the array is :");
+
+for(i of arr.keys())
+{
+    console.log(i);
+    
+}
+
+console.log("values of the array are :");
+
+
+//value give the value of the array
+
+for(j of arr.values())
+{
+    console.log(j);
+    
+}
