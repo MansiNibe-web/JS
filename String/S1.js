2e7b2f30df96df419c374549efe2ee8a682fd107
MansiNibe-web <mansinibe03@gmail.com>
2024-09-30 13:09:24 +0530

    String methods

---
+++ String/S1.js
@@ -0,0 +1,14 @@
+
+
+//String literal 
+
+
+const str1='I am the best';
+const str2="I am the best";
+const str3=`I am the best`;
+
+const str4=new String("I am the best !");
+const str5=String("str56");
+
+
+console.log(str1,str2,str3.str4,str5);
