d9151822758993f4187f86557b11fb5e137e256f
MansiNibe-web <mansinibe03@gmail.com>
2024-10-04 14:22:34 +0530

    string methods

---
+++ String/Upper.js
@@ -0,0 +1,12 @@
+//to upper case
+let nm="mansi";
+console.log(nm.toUpperCase());
+console.log(nm);
+
+
+//to lowercase
+let mn="NIBE";
+mn.toLowerCase();
+console.log(mn.toLowerCase());
+console.log(mn);
+
