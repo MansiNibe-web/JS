2524eb3028f7a3000f1dc200ee208daef8b3763f
MansiNibe-web <mansinibe03@gmail.com>
2024-10-04 14:33:19 +0530

    String methods

--- String/len.js
+++ String/len.js
@@ -2,3 +2,15 @@ let str="mansi nibe";
 console.log(str.length);
 
 console.log(str);
+//lenth is not a function 
+//we can directly apply on a String also on a variable also 
+
+//directly on a string 
+console.log("Mansi Sunil Nibe".length);
+
+//if we want to acceess a specific character from the string then we can do that by providing the index of that character
+//index of the string always start from 0
+let str2="mansi";
+console.log(str[3]);
+
+
