27b7dfff6e6c93f44e1f96618e65e089c4c86ed1
MansiNibe-web <mansinibe03@gmail.com>
2024-10-09 14:05:44 +0530

    for in loop

---
+++ Objects/forin.js
@@ -0,0 +1,15 @@
+let obj={
+    name:"masni",
+    age:'23',
+    dob:"03/05/2001",
+
+}
+for(let i in obj)
+{
+    console.log(i);
+    console.log(obj[i]);
+    
+    
+}
+//for in loop is used in object 
+//of  want to access the keys or values of an object the u can ue the for in loop 
