878ad3e380a40814cd61ee8edcfba6bd2143c675
MansiNibe-web <mansinibe03@gmail.com>
2024-10-09 14:09:16 +0530

    for of loop

---
+++ Objects/forof.js
@@ -0,0 +1,12 @@
+
+
+//for off loop 
+//used for string and array 
+//it iterrate on a specific character of a string 
+
+let str="mansi sunil nibe";
+for(let i of str)
+{
+    console.log("value : "+i);
+    
+}
